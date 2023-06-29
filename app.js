cb8fbddb26677eb990777b0c04546cca0f1193f1
CharlesChinedum <charleschinedum2@gmail.com>
2023-06-29 10:35:30 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET_STRING,
+  encryptedFields: ["password"],
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const userName = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: userName })
+      .then((foundUser) => {
+        if (foundUser) {
+          if (foundUser.password === password) {
+            res.render("secrets");
+          }
+        } else {
+          res.send("Invalid email or password");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+app
+  .route("/register")
+
+  .get((req, res) => {
+    res.render("register");
+  })
+
+  .post((req, res) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password,
+    });
+
+    newUser
+      .save()
+      .then(() => {
+        res.render("secrets");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+app.listen(3000, () => {
+  console.log("speak chef, I'm listening on port 3000");
+});
